95b6132d945713f3a40b0b598eb0303b67019864
gptshubham <shubhamca76@gmail.com>
2025-01-04 15:47:39 +0530

    added reset button functionality and some minor ui updates

--- script.js
+++ script.js
@@ -2,14 +2,27 @@ const billAmount = document.querySelector('#bill_amount');
 
 const allTipButtons = document.querySelectorAll('.tip-btn');
 
+// selecting grey inputs
+const greyInputs = document.querySelectorAll('.grey-input');
+
+const customTipAmountInput = document.querySelector('#custom-tip-amount');
+const tipButtonContainer = document.querySelector('.tip-btn-container');
+const numberOfPeopleInput = document.querySelector('#number-of-people');
 const generateBillButton = document.querySelector('.generate-bill-btn');
+
+const resetButton = document.querySelector('.reset-btn');
+
+const tipAmountOutput = document.querySelector('#tip-amount-output');
+
+const totalOutput = document.querySelector('#total-output');
+
+const eachPersonBillOutput = document.querySelector('#each-person-bill-output');
 
 // variable to store bill amount
 let bill = 0;
 
 // Event Listener for enabling disabled buttons and grey inputs based on the bill amount
 billAmount.addEventListener('input', (e) => {
-  // console.log(e.currentTarget.value);
   bill = Number(e.currentTarget.value);
 
   // unlocking buttons and other inputs if bill amount > 0
@@ -20,9 +33,6 @@ billAmount.addEventListener('input', (e) => {
       el.style.cursor = 'pointer';
     }
 
-    // selecting grey inputs
-    const greyInputs = document.querySelectorAll('.grey-input');
-
     // unlocking grey inputs
     for (const el of greyInputs) {
       // removing disabled attribute
@@ -31,27 +41,14 @@ billAmount.addEventListener('input', (e) => {
   }
 });
 
-// selecting tip-buttons-container
-const tipButtonContainer = document.querySelector('.tip-btn-container');
-// console.log(tipButtonContainer);
-
 // variable to store tip percentage value
 let tipPercentage = 0;
 // Event Listener for tip percentage buttons
 tipButtonContainer.addEventListener('click', (e) => {
-  // console.log(e.target.id);
   tipPercentage = Number(e.target.id);
-  // console.log(tipPercentage);
-  // console.log(typeof tipPercentage);
-
-  // orange border
-  e.target.style.border = '5px solid orange';
 });
 
 // Custom Tip Amount
-
-// selecting custom tip amount input element
-const customTipAmountInput = document.querySelector('#custom-tip-amount');
 
 // variable to store custom tip amount
 let customTipAmount = 0;
@@ -59,26 +56,22 @@ let customTipAmount = 0;
 // event listener to fetch the value of custom tip amount
 customTipAmountInput.addEventListener('change', (e) => {
   customTipAmount = Number(e.currentTarget.value);
-  // console.log(customTipAmount);
 });
 
 // Number of People
-
-const numberOfPeopleInput = document.querySelector('#number-of-people');
 
 let numberOfPeople = 1;
 
 numberOfPeopleInput.addEventListener('input', (e) => {
   if (Number(e.currentTarget.value) > 0) {
     numberOfPeople = Number(e.currentTarget.value);
-    console.log(numberOfPeople);
   }
 
   // unlocking generate bill button
   generateBillButton.removeAttribute('disabled');
 });
 
-// Generate Bill Button
+// Generate Bill Button Functionality
 
 generateBillButton.addEventListener('click', (e) => {
   // tip amount
@@ -90,25 +83,40 @@ generateBillButton.addEventListener('click', (e) => {
   } else {
     tipAmount = 0;
   }
-  console.log(tipAmount);
 
   // total
   const total = bill + tipAmount;
 
-  console.log(total);
-
   // each person bill
   const eachPersonBill = Math.round(total / numberOfPeople);
-  console.log(eachPersonBill);
 
-  const tipAmountOutput = document.querySelector('#tip-amount-output');
   tipAmountOutput.textContent = tipAmount;
 
-  const totalOutput = document.querySelector('#total-output');
   totalOutput.textContent = total;
 
-  const eachPersonBillOutput = document.querySelector(
-    '#each-person-bill-output'
-  );
   eachPersonBillOutput.textContent = eachPersonBill;
+
+  // unlocking reset button
+  resetButton.removeAttribute('disabled');
+  resetButton.style.cursor = 'pointer';
+  resetButton.style.cursor = 'default';
+});
+
+// Reset Button functionality
+resetButton.addEventListener('click', (e) => {
+  billAmount.value = '';
+  customTipAmountInput.value = '';
+  customTipAmountInput.disabled = true;
+  numberOfPeopleInput.value = '';
+  numberOfPeopleInput.disabled = true;
+  generateBillButton.disabled = true;
+
+  for (const el of allTipButtons) {
+    el.style.opacity = '0.6';
+    el.style.cursor = 'not-allowed';
+  }
+
+  tipAmountOutput.textContent = '';
+  totalOutput.textContent = '';
+  eachPersonBillOutput.textContent = '';
 });
