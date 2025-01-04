a91e72b7373e3894e5697c8c5039c5d9e8e7edf6
gptshubham <shubhamca76@gmail.com>
2025-01-04 13:09:40 +0530

    added event listeners to fetch tip %, custom tip amount and number of people

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
-const root = document.querySelector('main');
-// console.log(root);
-
 const billAmount = document.querySelector('#bill_amount');
-// console.log(billAmount);
 
 const allTipButtons = document.querySelectorAll('.tip-btn');
-// console.log(allTipButtons);
-
-const customTipPercentageInput = document.querySelector(
-  '#custom-tip-percentage'
-);
-// console.log(customTipPercentageInput);
-
-const numberOfPeopleInput = document.querySelector('#number-of-people');
-// console.log(numberOfPeopleInput);
 
 const generateBillButton = document.querySelector('.generate-bill-btn');
-// console.log(generateBillButton);
 
+// variable to store bill amount
+let bill = 0;
+
+// Event Listener for enabling disabled buttons and grey inputs based on the bill amount
 billAmount.addEventListener('input', (e) => {
   // console.log(e.currentTarget.value);
-  const bill = Number(e.currentTarget.value);
+  bill = Number(e.currentTarget.value);
 
   // unlocking buttons and other inputs if bill amount > 0
   if (bill > 0) {
@@ -40,3 +30,44 @@ billAmount.addEventListener('input', (e) => {
     }
   }
 });
+
+// selecting tip-buttons-container
+const tipButtonContainer = document.querySelector('.tip-btn-container');
+// console.log(tipButtonContainer);
+
+// variable to store tip percentage value
+let tipPercentage = 0;
+// Event Listener for tip percentage buttons
+tipButtonContainer.addEventListener('click', (e) => {
+  // console.log(e.target.id);
+  tipPercentage = Number(e.target.id);
+  console.log(tipPercentage);
+  // console.log(typeof tipPercentage);
+});
+
+// Custom Tip Amount
+
+// selecting custom tip amount input element
+const customTipAmountInput = document.querySelector('#custom-tip-amount');
+
+// variable to store custom tip amount
+let customTipAmount = 0;
+
+// event listener to fetch the value of custom tip amount
+customTipAmountInput.addEventListener('change', (e) => {
+  customTipAmount = Number(e.currentTarget.value);
+  // console.log(customTipAmount);
+});
+
+// Number of People
+
+const numberOfPeopleInput = document.querySelector('#number-of-people');
+
+let numberOfPeople = 0;
+
+numberOfPeopleInput.addEventListener('change', (e) => {
+  numberOfPeople = Number(e.currentTarget.value);
+  console.log(numberOfPeople);
+});
+
+// Generate Bill Button
